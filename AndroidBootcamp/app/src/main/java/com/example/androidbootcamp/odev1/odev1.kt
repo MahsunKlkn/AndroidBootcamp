1d3fd954602d1bc02b32401d293f5f9496a65955
Mahsun <mhsnkalkan@gmail.com>
2025-04-26 23:16:57 +0300

    Odev1

---
+++ AndroidBootcamp/app/src/main/java/com/example/androidbootcamp/odev1/odev1.kt
@@ -0,0 +1,190 @@
+package com.example.androidbootcamp.odev1
+
+import java.util.Date
+
+fun main() {
+    println("Hello, world!!!")
+    println("Merhaba")
+
+    //-------------  1 ----------//
+    val sehir:String
+    sehir ="Adana"
+    println(sehir)
+    //-------------  2 ----------//
+    val ulke:String
+    ulke="Türkiye"
+    println(ulke)
+    //-------------  3 ----------//
+    val telefon:Long
+    telefon=5554445235
+    println(telefon)
+    //-------------  4 ----------//
+    val postaKodu:Long
+    postaKodu=1050
+    println(postaKodu)
+    //-------------  5 ----------//
+    val email:String
+    email="mhsnkalkan@gmail.com"
+    println(email)
+    //-------------  6 ----------//
+    val meslek:String
+    meslek="Bilgisayar Mühendisi"
+    println(meslek)
+    //-------------  7 ----------//
+    val stokMiktari:Int
+    stokMiktari=544
+    println(stokMiktari)
+    //-------------  8 ----------//
+    val musteriAdi:String
+    musteriAdi="Mahsun"
+    println(musteriAdi)
+    //-------------  9 ----------//
+    val bakiye:Int
+    bakiye=100000
+    println(bakiye)
+    //-------------  10 ----------//
+    val dogumGunu:String
+    dogumGunu="09.02.2001"
+    println(dogumGunu)
+    //-------------  11 ----------//
+    val maas:Int
+    maas=700000
+    println(maas)
+    //-------------  12 ----------//
+    val medeniDurum:String
+    medeniDurum="Bekar"
+    println(medeniDurum)
+    //-------------  13 ----------//
+    val urunYorum:String
+    urunYorum="Merhaba bu ilk kotlin calismam"
+    println(urunYorum)
+    //-------------  14 ----------//
+    val odemeTarihi:String
+    odemeTarihi="09.02.2031"
+    println(odemeTarihi)
+    //-------------  15 ----------//
+    val odeme:String
+    odeme="Karttan"
+    println(odeme)
+    //-------------  16 ----------//
+    val siparisAdedi:Int
+    siparisAdedi=453
+    println(siparisAdedi)
+    //-------------  17 ----------//
+    val arabaModeli:String
+    arabaModeli="Range Rover"
+    println(arabaModeli)
+    //-------------  18 ----------//
+    val kitapAdi:String
+    kitapAdi="Seker Portakali"
+    println(kitapAdi)
+    //-------------  19 ----------//
+    val yayinlanmaTarihi:String
+    yayinlanmaTarihi="09.02.2031"
+    println(yayinlanmaTarihi)
+    //-------------  20 ----------//
+    val odaSayisi:Int
+    odaSayisi=453
+    println(odaSayisi)
+    //-------------  21 ----------//
+    val enlem:Int
+    enlem=5
+    println(enlem)
+    //-------------  22 ----------//
+    val boylam:Int
+    boylam=8
+    println(boylam)
+    //-------------  23 ----------//
+    val urunAdi:String
+    urunAdi="Kitap"
+    println(urunAdi)
+    //-------------  24 ----------//
+    val yemekFiyati:Int
+    yemekFiyati=219
+    println(yemekFiyati)
+    //-------------  25 ----------//
+    val marka:String
+    marka="Iphone 16"
+    println(marka)
+    //-------------  26 ----------//
+    val muzikAdi:String
+    muzikAdi="Die for You"
+    println(muzikAdi)
+    //-------------  27 ----------//
+    val videoSuresi:Int
+    videoSuresi=67
+    println(videoSuresi)
+    //-------------  28 ----------//
+    val urunPuani:Int
+    urunPuani=4
+    println(urunPuani)
+    //-------------  29 ----------//
+    val resimAdi:String
+    resimAdi="Kaplumbağa Terbiyecisi"
+    println(resimAdi)
+    //-------------  30 ----------//
+    val dosyaFormati:String
+    dosyaFormati="png"
+    println(dosyaFormati)
+    //-------------  31 ----------//
+    val renk:String
+    renk="Beyaz"
+    println(renk)
+    //-------------  32 ----------//
+    val renkKodu:String
+    renkKodu="#FFFFFF"
+    println(renkKodu)
+    //-------------  33 ----------//
+    val telefonModeli:String
+    telefonModeli="Realme C55"
+    println(telefonModeli)
+    //-------------  34 ----------//
+    val ekranBoyutu:Int
+    ekranBoyutu=12
+    println(ekranBoyutu)
+    //-------------  35 ----------//
+    val agirlik:Int
+    agirlik=90
+    println(agirlik)
+    //-------------  36 ----------//
+    val ulusalGun:String
+    ulusalGun="10.10.1938"
+    println(ulusalGun)
+    //-------------  37 ----------//
+    val tatilGunu:String
+    tatilGunu="1.05.2025"
+    println(tatilGunu)
+    //-------------  38 ----------//
+    val rezervasyonTarihi:String
+    rezervasyonTarihi="30.04.2025"
+    println(rezervasyonTarihi)
+    //-------------  39 ----------//
+    val sokakAdi:String
+    sokakAdi="Çikmaz"
+    println(sokakAdi)
+    //-------------  40 ----------//
+    val otobusHatti:String
+    otobusHatti="Onderler"
+    println(otobusHatti)
+    //-------------  41 ----------//
+    val kalanDakika:Int
+    kalanDakika=5
+    println(kalanDakika)
+    //-------------  42 ----------//
+    val takipKodu:Int
+    takipKodu=1245486
+    println(takipKodu)
+    //-------------  43 ----------//
+    val kuponSuresi:Int
+    kuponSuresi=12
+    println(kuponSuresi)
+    //-------------  44 ----------//
+    val kuponKodu:Int
+    kuponKodu=223020100
+    println(kuponKodu)
+    //-------------  45 ----------//
+    val faturaAdresi:String
+    faturaAdresi="Adana Seyhan Bilinmez Sokak"
+    println(faturaAdresi)
+
+}
